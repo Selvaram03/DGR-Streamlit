e3d17eaf40085a9790c3385f4fd8599e54772e6c
Selva rama bala subramanian S <122852256+Selvaram03@users.noreply.github.com>
2025-10-16 15:59:16 +0530

    PSS data updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ CUSTOMER_TABLES = {
     "Mauryaa": "Mauryaa",
     "Paranjape": "Paranjape",
     "BEL1": "Rajgir",
-    "Vinathi_2": "PSS"
+    "Vinathi_2": "PSS",
     "Vinathi_3": "Vinathi_3",
     "Vinathi_4": "Vinathi_4",
     "TMD": "TMD",
@@ -184,5 +184,6 @@ elif st.session_state.page == "live":
     summary_df = pd.DataFrame(summary_list)
     st.dataframe(summary_df, width="stretch")
     st.caption(f"Last refreshed at {datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')} IST")
+
 
 
