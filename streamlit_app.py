a920b73e5153ffdaa72399e7372449f57efbae6b
Selva rama bala subramanian S <122852256+Selvaram03@users.noreply.github.com>
2025-10-31 11:12:30 +0530

    Refactor streamlit_app.py for improved structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ if st.session_state.page == "report":
     start_str = month_start.strftime("%d-%b-%Y")
     end_str = report_date.strftime("%d-%b-%Y")
 
-    logger.info(
+    # logger.info(
         f"🔍 Fetching MongoDB data → Customer={customer}, Collection={collection_name}, Start={start_str}, End={end_str}"
     )
 
@@ -219,4 +219,5 @@ else:
     summary_df = pd.DataFrame(summary_list)
     st.dataframe(summary_df)
     st.caption(f"Last refreshed at {datetime.now(IST).strftime('%Y-%m-%d %H:%M:%S')} IST")
+
 
